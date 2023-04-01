d90abe9070d9b55b4e6c72658467360613a44baf
OmerRonen <omerronen10@gmail.com>
2023-04-01 14:02:21 -0700

    running time analysis

--- config/shrinkage/ccp_expr.py
+++ config/shrinkage/ccp_expr.py
@@ -7,9 +7,9 @@ import time
 from sklearn.model_selection import train_test_split
 from imodels import get_clean_dataset, HSTreeRegressorCV
 from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 
-from datasets import DATASETS_REGRESSION
+from datasets import DATASETS_REGRESSION, DATASETS_CLASSIFICATION
 
 
 
@@ -28,36 +28,60 @@ def cv_ccp(tree, X_train, y_train):
     return final_reg
 
 
+from sklearn.metrics import mean_squared_error, roc_auc_score
+
+
+def evaluate_estimator(estimator, X, y):
+    """
+    Evaluate a scikit-learn estimator and return MSE or AUC depending on the estimator type.
+
+    Parameters:
+        estimator (object): A trained scikit-learn estimator.
+        X (array-like): Input data to use for prediction.
+        y (array-like): Ground truth labels.
+
+    Returns:
+        float: Mean squared error (MSE) if the estimator is a regressor, or area under the ROC curve (AUC) if the estimator is a binary classifier.
+    """
+    if "Classifier" in estimator.__class__.__name__:
+        # If the estimator is a binary classifier, use AUC
+        y_pred_proba = estimator.predict_proba(X)[:, 1]
+        return roc_auc_score(y, y_pred_proba)
+    else:
+        # If the estimator is a regressor, use MSE
+        y_pred = estimator.predict(X)
+        return mean_squared_error(y, y_pred)
+
+
 def main():
     # get the data
     performance = {"ccp":[], "tv": []}
     running_time = {"ccp":[], "tv": []}
     data_sizes = []
-
-    for d in DATASETS_REGRESSION:
-        X, y, feat_names = get_clean_dataset(d[1], data_source=d[2])
-        # train test split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        data_sizes.append(X_train.shape[0])
-        # fit a decision tree
-        tree = DecisionTreeRegressor(random_state=42)
-        tree.fit(X_train, y_train)
-        # get cart test rmse
-        mse_cart = mean_squared_error(y_test, tree.predict(X_test))
-        # perform ccp pruning and get the time and test performance (mean squared error)
-        t0 = time.time()
-        tree_ccp = cv_ccp(tree, X_train, y_train)
-        ccp_time = time.time() - t0
-        running_time['ccp'].append(ccp_time)
-        mse_ccp = mean_squared_error(y_test, tree_ccp.predict(X_test)) / mse_cart
-        performance['ccp'].append(mse_ccp)
-        # do tv shrinkage and get time and test performance
-        t2 = time.time()
-        tree_tv = HSTreeRegressorCV(copy.deepcopy(tree), shrinkage_scheme_="tv")
-        tv_shrink_time = time.time() - t2
-        running_time['tv'].append(tv_shrink_time)
-        mse_tv = mean_squared_error(y_test, tree_tv.predict(X_test)) / mse_cart
-        performance['tv'].append(mse_tv)
+    ests = {0: DecisionTreeRegressor, 1: DecisionTreeClassifier}
+    for problem_type, datasets in enumerate([DATASETS_REGRESSION, DATASETS_CLASSIFICATION]):
+        for d in DATASETS_REGRESSION:
+            X, y, feat_names = get_clean_dataset(d[1], data_source=d[2])
+            # train test split
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            data_sizes.append(X_train.shape[0])
+            # fit a decision tree
+            tree = ests[problem_type](random_state=42)#DecisionTreeRegressor(random_state=42)
+            tree.fit(X_train, y_train)
+            # calculate area under roc curve
+            cart_perf = evaluate_estimator(tree , X_test, y_test)
+            # perform ccp pruning and get the time and test performance (mean squared error)
+            t0 = time.time()
+            tree_ccp = cv_ccp(tree, X_train, y_train)
+            ccp_time = time.time() - t0
+            running_time['ccp'].append(ccp_time)
+            performance['ccp'].append(evaluate_estimator(tree_ccp, X_test, y_test)/cart_perf)
+            # do tv shrinkage and get time and test performance
+            t2 = time.time()
+            tree_tv = HSTreeRegressorCV(copy.deepcopy(tree), shrinkage_scheme_="tv")
+            tv_shrink_time = time.time() - t2
+            running_time['tv'].append(tv_shrink_time)
+            performance['tv'].append(evaluate_estimator(tree_tv, X_test, y_test)/cart_perf)
 
     # save the performance and running time to a single json file
     import json
